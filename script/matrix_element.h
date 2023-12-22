15dd001ec658151c3f734e7bb33d05d1c6600d56
Kotaro Sonoda <sonoda.ktr@gmail.com>
2023-12-22 16:42:48 +0100

    refactor: Move matrix element out as a header.

---
+++ script/matrix_element.h
@@ -0,0 +1,12 @@
+//
+//  matrix_element.h
+//  molecular_orientation_control
+//
+//  Created by Kotaro Sonoda on 22/12/2023.
+//
+
+#ifndef matrix_element_h
+#define matrix_element_h
+
+
+#endif /* matrix_element_h */
